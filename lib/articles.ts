0ca7b00a3566882b6681d4acb51274850d997385
LOPPIN Nicolas <loppin.n66@gmail.com>
2025-07-14 15:22:27 +0200

    Repair build

--- lib/articles.ts
+++ lib/articles.ts
@@ -6,7 +6,6 @@ import { remark } from "remark";
 import matter from "gray-matter";
 
 import type { ArticleItem } from "@/types";
-import { title } from "process";
 
 const articlesDirectory = path.join(process.cwd(), "articles");
 
